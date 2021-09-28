dd1b5c1a60b7b89ab3cdfeaccbd241beecd20dd2
FinagleLord <72840971+FinagleLord@users.noreply.github.com>
2021-09-27 23:15:45 -0500

    Create StakedOHM.sol

---
+++ StakedOHM.sol
@@ -0,0 +1,549 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-06-12
+*/
+
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.5;
+
+library SafeMath {
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+
+    function sqrrt(uint256 a) internal pure returns (uint c) {
+        if (a > 3) {
+            c = a;
+            uint b = add( div( a, 2), 1 );
+            while (b < c) {
+                c = b;
+                b = div( add( div( a, b ), b), 2 );
+            }
+        } else if (a != 0) {
+            c = 1;
+        }
+    }
+
+    /*
+     * Expects percentage to be trailed by 00,
+    */
+    function percentageAmount( uint256 total_, uint8 percentage_ ) internal pure returns ( uint256 percentAmount_ ) {
+        return div( mul( total_, percentage_ ), 1000 );
+    }
+
+    /*
+     * Expects percentage to be trailed by 00,
+    */
+    function substractPercentage( uint256 total_, uint8 percentageToSub_ ) internal pure returns ( uint256 result_ ) {
+        return sub( total_, div( mul( total_, percentageToSub_ ), 1000 ) );
+    }
+
+    function percentageOfTotal( uint256 part_, uint256 total_ ) internal pure returns ( uint256 percent_ ) {
+        return div( mul(part_, 100) , total_ );
+    }
+
+    /**
+     * Taken from Hypersonic https://github.com/M2629/HyperSonic/blob/main/Math.sol
+     * @dev Returns the average of two numbers. The result is rounded towards
+     * zero.
+     */
+    function average(uint256 a, uint256 b) internal pure returns (uint256) {
+        // (a + b) / 2 can overflow, so we distribute
+        return (a / 2) + (b / 2) + ((a % 2 + b % 2) / 2);
+    }
+
+    function quadraticPricing( uint256 payment_, uint256 multiplier_ ) internal pure returns (uint256) {
+        return sqrrt( mul( multiplier_, payment_ ) );
+    }
+
+  function bondingCurve( uint256 supply_, uint256 multiplier_ ) internal pure returns (uint256) {
+      return mul( multiplier_, supply_ );
+  }
+}
+
+library Address {
+
+    function isContract(address account) internal view returns (bool) {
+        uint256 size;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { size := extcodesize(account) }
+        return size > 0;
+    }
+
+    function functionCall(address target, bytes memory data) internal returns (bytes memory) {
+        return _functionCallWithValue(target, data, 0, "Address: low-level call failed");
+    }
+
+    function _functionCallWithValue(address target, bytes memory data, uint256 weiValue, string memory errorMessage) private returns (bytes memory) {
+        require(isContract(target), "Address: call to non-contract");
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = target.call{ value: weiValue }(data);
+        if (success) {
+            return returndata;
+        } else {
+            // Look for revert reason and bubble it up if present
+            if (returndata.length > 0) {
+                // The easiest way to bubble the revert reason is using memory via assembly
+
+                // solhint-disable-next-line no-inline-assembly
+                assembly {
+                    let returndata_size := mload(returndata)
+                    revert(add(32, returndata), returndata_size)
+                }
+            } else {
+                revert(errorMessage);
+            }
+        }
+    }
+
+    function _verifyCallResult(bool success, bytes memory returndata, string memory errorMessage) private pure returns(bytes memory) {
+        if (success) {
+            return returndata;
+        } else {
+            // Look for revert reason and bubble it up if present
+            if (returndata.length > 0) {
+                // The easiest way to bubble the revert reason is using memory via assembly
+
+                // solhint-disable-next-line no-inline-assembly
+                assembly {
+                    let returndata_size := mload(returndata)
+                    revert(add(32, returndata), returndata_size)
+                }
+            } else {
+                revert(errorMessage);
+            }
+        }
+    }
+}
+
+interface IERC20 {
+  function totalSupply() external view returns (uint256);
+  function balanceOf(address account) external view returns (uint256);
+  function transfer(address recipient, uint256 amount) external returns (bool);
+  function allowance(address owner, address spender) external view returns (uint256);
+  function approve(address spender, uint256 amount) external returns (bool);
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+interface IOwnable {
+  function manager() external view returns (address);
+  function renounceManagement() external;
+  function pushManagement( address newOwner_ ) external;
+  function pullManagement() external;
+}
+
+contract Ownable is IOwnable {
+
+    address internal _owner;
+    address internal _newOwner;
+
+    event OwnershipPushed(address indexed previousOwner, address indexed newOwner);
+    event OwnershipPulled(address indexed previousOwner, address indexed newOwner);
+
+    constructor () {
+        _owner = msg.sender;
+        emit OwnershipPushed( address(0), _owner );
+    }
+
+    function manager() public view override returns (address) {
+        return _owner;
+    }
+
+    modifier onlyManager() {
+        require( _owner == msg.sender, "Ownable: caller is not the owner" );
+        _;
+    }
+
+    function renounceManagement() public virtual override onlyManager() {
+        emit OwnershipPushed( _owner, address(0) );
+        _owner = address(0);
+    }
+
+    function pushManagement( address newOwner_ ) public virtual override onlyManager() {
+        require( newOwner_ != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipPushed( _owner, newOwner_ );
+        _newOwner = newOwner_;
+    }
+    
+    function pullManagement() public virtual override {
+        require( msg.sender == _newOwner, "Ownable: must be new owner to pull");
+        emit OwnershipPulled( _owner, _newOwner );
+        _owner = _newOwner;
+    }
+}
+
+interface IOracle {
+    function getPrice( address _pool ) external returns ( uint );
+}
+
+/// @author Dionysus
+contract Domain {
+
+    bytes32 public DOMAIN_SEPARATOR;
+
+    bytes32 public DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
+
+    string private EIP191_PREFIX = "\x19\x01";
+
+    function _getDigest(
+        bytes32 hash
+            ) internal view returns (bytes32 digest) {
+                digest = keccak256(abi.encodePacked(EIP191_PREFIX, DOMAIN_SEPARATOR, hash));
+    }
+
+    function _getChainId(
+        ) internal view returns (uint) {
+            uint256 chainId;
+            assembly { chainId := chainid() }
+            return chainId;
+    }
+
+    function version(
+        ) public pure virtual returns(string memory){
+            return "1"; 
+    }
+}
+
+contract sOlympus is Ownable, Domain {
+
+    /* ========== DEPENDENCIES ========== */
+
+    using SafeMath for uint256;
+
+
+
+    /* ========== EVENTS ========== */
+
+    event LogSupply(uint256 indexed epoch, uint256 timestamp, uint256 totalSupply );
+    event LogRebase( uint256 indexed epoch, uint256 rebase, uint256 index );
+    event LogStakingContractUpdated( address stakingContract );
+
+
+
+    /* ========== MODIFIERS ========== */
+
+    modifier onlyStakingContract() {
+        require( msg.sender == stakingContract );
+        _;
+    }
+
+
+    /* ========== DATA STRUCTURES ========== */
+
+    struct Rebase {
+        uint epoch;
+        uint rebase; // 18 decimals
+        uint totalStakedBefore;
+        uint totalStakedAfter;
+        uint amountRebased;
+        uint index;
+        uint indexAdjustedPrice;
+        uint blockNumberOccured;
+    }
+
+
+
+    /* ========== STATE VARIABLES ========== */
+
+    bytes32 public immutable PERMIT_TYPEHASH = keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+
+    address initializer;
+
+    // Index Gons - tracks rebase growth
+    uint INDEX;
+    
+    // balance used to calc rebase
+    address public stakingContract; 
+
+    // pulls price from pool
+    IOracle oracle;
+    
+    address pool;
+
+    // past rebase data    
+    Rebase[] public rebases; 
+
+    uint256 private constant MAX_UINT256 = ~uint256(0);
+    
+    uint256 private constant INITIAL_FRAGMENTS_SUPPLY = 5000000 * 10**9;
+
+    // TOTAL_GONS is a multiple of INITIAL_FRAGMENTS_SUPPLY so that _gonsPerFragment is an integer.
+    // Use the highest value that fits in a uint256 for max granularity.
+    uint256 private constant TOTAL_GONS = MAX_UINT256 - (MAX_UINT256 % INITIAL_FRAGMENTS_SUPPLY);
+
+    // MAX_SUPPLY = maximum integer < (sqrt(4*TOTAL_GONS + 1) - 1) / 2
+    uint256 private constant MAX_SUPPLY = ~uint128(0);  // (2^128) - 1
+
+    uint256 private _gonsPerFragment;
+    
+    mapping(address => uint256) private _gonBalances;
+
+    mapping ( address => mapping ( address => uint256 ) ) private _allowedValue;
+
+
+
+    /* ========== CONSTRUCTOR ========== */
+
+    constructor() ERC20("Staked Olympus", "sOHM", 9) {
+        initializer = msg.sender;
+        _totalSupply = INITIAL_FRAGMENTS_SUPPLY;
+        _gonsPerFragment = TOTAL_GONS.div(_totalSupply);
+        
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                DOMAIN_TYPEHASH,
+                keccak256(bytes(name())),
+                keccak256(bytes(version())),
+                _getChainId(),
+                address(this)
+            )
+        );
+    }
+
+
+
+    /* ========== INITIALIZATION ========== */
+
+    function initialize( address stakingContract_ ) external {
+        require( msg.sender == initializer );
+
+        require( stakingContract_ != address(0) );
+        stakingContract = stakingContract_;
+        _gonBalances[ stakingContract ] = TOTAL_GONS;
+
+        emit Transfer( address(0x0), stakingContract, _totalSupply );
+        emit LogStakingContractUpdated( stakingContract_ );
+        
+        initializer = address(0);
+    }
+
+    function setIndex( uint _INDEX ) external onlyManager() {
+        require( INDEX == 0 );
+        INDEX = gonsForBalance( _INDEX );
+    }
+
+    function setPool( address _oracle, address _pool ) external onlyManager() {
+        oracle = IOracle( _oracle );
+        pool = _pool;
+    }
+
+
+
+    /* ========== REBASE ========== */
+
+    /**
+        @notice increases sOHM supply to increase staking balances relative to profit_
+        @param profit_ uint256
+        @return uint256
+     */
+    function rebase( uint256 profit_, uint epoch_ ) public onlyStakingContract() returns ( uint256 ) {
+        uint256 rebaseAmount;
+        uint256 circulatingSupply_ = circulatingSupply();
+
+        if ( profit_ == 0 ) {
+            emit LogSupply( epoch_, block.timestamp, _totalSupply );
+            emit LogRebase( epoch_, 0, index() );
+            return _totalSupply;
+        } else if ( circulatingSupply_ > 0 ){
+            rebaseAmount = profit_.mul( _totalSupply ).div( circulatingSupply_ );
+        } else {
+            rebaseAmount = profit_;
+        }
+
+        _totalSupply = _totalSupply.add( rebaseAmount );
+
+        if ( _totalSupply > MAX_SUPPLY ) {
+            _totalSupply = MAX_SUPPLY;
+        }
+
+        _gonsPerFragment = TOTAL_GONS.div( _totalSupply );
+
+        _storeRebase( circulatingSupply_, profit_, epoch_ );
+
+        return _totalSupply;
+    }
+
+    /**
+        @notice emits event with data about rebase
+        @param previousCirculating_ uint
+        @param profit_ uint
+        @param epoch_ uint
+     */
+    function _storeRebase( uint previousCirculating_, uint profit_, uint epoch_ ) internal {
+        uint rebasePercent = profit_.mul( 1e18 ).div( previousCirculating_ );
+
+        rebases.push( Rebase ( {
+            epoch: epoch_,
+            rebase: rebasePercent, // 18 decimals
+            totalStakedBefore: previousCirculating_,
+            totalStakedAfter: circulatingSupply(),
+            amountRebased: profit_,
+            index: index(),
+            indexAdjustedPrice: index().mul( oracle.getPrice( pool ) ).div( 1e9 ),
+            blockNumberOccured: block.number
+        }));
+        
+        emit LogSupply( epoch_, block.timestamp, _totalSupply );
+        emit LogRebase( epoch_, rebasePercent, index() );
+    }
+
+
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    function transfer( address to, uint256 value ) public override returns (bool) {
+        uint256 gonValue = value.mul( _gonsPerFragment );
+        _gonBalances[ msg.sender ] = _gonBalances[ msg.sender ].sub( gonValue );
+        _gonBalances[ to ] = _gonBalances[ to ].add( gonValue );
+        emit Transfer( msg.sender, to, value );
+        return true;
+    }
+
+    function transferFrom( address from, address to, uint256 value ) public override returns (bool) {
+       _allowedValue[ from ][ msg.sender ] = _allowedValue[ from ][ msg.sender ].sub( value );
+       emit Approval( from, msg.sender,  _allowedValue[ from ][ msg.sender ] );
+
+        uint256 gonValue = gonsForBalance( value );
+        _gonBalances[ from ] = _gonBalances[from].sub( gonValue );
+        _gonBalances[ to ] = _gonBalances[to].add( gonValue );
+        emit Transfer( from, to, value );
+        return true;
+    }
+
+    function approve( address spender, uint256 value ) public override returns (bool) {
+         _allowedValue[ msg.sender ][ spender ] = value;
+         emit Approval( msg.sender, spender, value );
+         return true;
+    }
+
+    function increaseAllowance( address spender, uint256 addedValue ) public override returns (bool) {
+        _allowedValue[ msg.sender ][ spender ] = _allowedValue[ msg.sender ][ spender ].add( addedValue );
+        emit Approval( msg.sender, spender, _allowedValue[ msg.sender ][ spender ] );
+        return true;
+    }
+
+    function decreaseAllowance( address spender, uint256 subtractedValue ) public override returns (bool) {
+        uint256 oldValue = _allowedValue[ msg.sender ][ spender ];
+        if (subtractedValue >= oldValue) {
+            _allowedValue[ msg.sender ][ spender ] = 0;
+        } else {
+            _allowedValue[ msg.sender ][ spender ] = oldValue.sub( subtractedValue );
+        }
+        emit Approval( msg.sender, spender, _allowedValue[ msg.sender ][ spender ] );
+        return true;
+    }
+
+    /*
+     * @dev sign an allowance offchain
+     */
+    function permit(
+        address owner, 
+        address spender, 
+        uint256 amount, 
+        uint256 deadline, 
+        uint8 v, 
+        bytes32 r, 
+        bytes32 s
+    ) external virtual {
+            require(deadline >= block.timestamp, "Permit: expired deadline");
+            
+            bytes32 hashStruct = keccak256(
+                abi.encode(
+                    PERMIT_TYPEHASH,
+                    owner,
+                    spender,
+                    amount,
+                    nonces[owner]++,
+                    deadline
+                )
+            );
+            
+            address signer = ecrecover(_getDigest(hashStruct), v, r, s);
+            require(signer != address(0) && signer == owner, "Permit: invalid signature");
+            _approve(owner, spender, amount);
+    }
+
+    /* ========== INTERNAL FUNCTIONS ========== */
+
+    // called in a permit
+    function _approve( address owner, address spender, uint256 value ) internal override virtual {
+        _allowedValue[owner][spender] = value;
+        emit Approval( owner, spender, value );
+    }
+
+
+
+    /* ========== VIEW FUNCTIONS ========== */
+
+    function balanceOf( address who ) public view override returns ( uint256 ) {
+        return _gonBalances[ who ].div( _gonsPerFragment );
+    }
+
+    function gonsForBalance( uint amount ) public view returns ( uint ) {
+        return amount.mul( _gonsPerFragment );
+    }
+
+    function balanceForGons( uint gons ) public view returns ( uint ) {
+        return gons.div( _gonsPerFragment );
+    }
+
+    // Staking contract holds excess sOHM
+    function circulatingSupply() public view returns ( uint ) {
+        return _totalSupply.sub( balanceOf( stakingContract ) );
+    }
+
+    function index() public view returns ( uint ) {
+        return balanceForGons( INDEX );
+    }
+
+    function allowance( address owner_, address spender ) public view override returns ( uint256 ) {
+        return _allowedValue[ owner_ ][ spender ];
+    }
+}
