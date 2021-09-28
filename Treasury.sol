43b03f08a95e65c60b3093efeb4c115aeaff1747
FinagleLord <72840971+FinagleLord@users.noreply.github.com>
2021-09-27 23:13:30 -0500

    Create Treasury.sol

---
+++ Treasury.sol
@@ -0,0 +1,441 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-05-28
+*/
+
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.5;
+
+library SafeMath {
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        return c;
+    }
+}
+
+library Address {
+
+  function isContract(address account) internal view returns (bool) {
+        // This method relies in extcodesize, which returns 0 for contracts in
+        // construction, since the code is only stored at the end of the
+        // constructor execution.
+
+        uint256 size;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { size := extcodesize(account) }
+        return size > 0;
+    }
+
+    function functionCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) {
+        return _functionCallWithValue(target, data, 0, errorMessage);
+    }
+
+    function _functionCallWithValue(address target, bytes memory data, uint256 weiValue, string memory errorMessage) private returns (bytes memory) {
+        require(isContract(target), "Address: call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = target.call{ value: weiValue }(data);
+        if (success) {
+            return returndata;
+        } else {
+            if (returndata.length > 0) {
+                // solhint-disable-next-line no-inline-assembly
+                assembly {
+                    let returndata_size := mload(returndata)
+                    revert(add(32, returndata), returndata_size)
+                }
+            } else {
+                revert(errorMessage);
+            }
+        }
+    }
+
+    function _verifyCallResult(bool success, bytes memory returndata, string memory errorMessage) private pure returns(bytes memory) {
+        if (success) {
+            return returndata;
+        } else {
+            if (returndata.length > 0) {
+                // solhint-disable-next-line no-inline-assembly
+                assembly {
+                    let returndata_size := mload(returndata)
+                    revert(add(32, returndata), returndata_size)
+                }
+            } else {
+                revert(errorMessage);
+            }
+        }
+    }
+}
+
+interface IQueueable {
+  function queuer() external view returns (address);
+  
+  function pushQueuer( address newOwner_ ) external;
+  
+  function pullQueuer() external;
+}
+
+contract Queueable is IQueueable {
+
+    address internal _queuer;
+    address internal _newQueuer;
+
+    event QueuerPushed(address indexed previousQueuer, address indexed newQueuer );
+    event QueuerPulled(address indexed previousQueuer, address indexed newQueuer );
+
+    constructor () {
+        _queuer = msg.sender;
+        emit QueuerPushed( address(0), _queuer );
+    }
+
+    function queuer() public view override returns (address) {
+        return _queuer;
+    }
+
+    modifier onlyQueuer() {
+        require( _queuer == msg.sender, "Ownable: caller is not the owner" );
+        _;
+    }
+
+    function pushQueuer( address newQueuer_ ) public virtual override onlyQueuer() {
+        require( newQueuer_ != address(0), "Ownable: new owner is the zero address");
+        emit QueuerPushed( _queuer, newQueuer_ );
+        _newQueuer = newQueuer_;
+    }
+    
+    function pullQueuer() public virtual override {
+        require( msg.sender == _newQueuer, "Ownable: must be new owner to pull");
+        emit QueuerPulled( _queuer, _newQueuer );
+        _queuer = _newQueuer;
+    }
+}
+
+interface IExecutable {
+  function executor() external view returns (address);
+  
+  function pushExecutor( address newExecutor_ ) external;
+  
+  function pullExecutor() external;
+}
+
+contract Executable is IExecutable {
+
+    address internal _executor;
+    address internal _newExecutor;
+
+    event ExecutorPushed(address indexed previousExecutor, address indexed newExecutor );
+    event ExecutorPulled(address indexed previousExecutor, address indexed newExecutor );
+
+    constructor () {
+        _executor = msg.sender;
+        emit ExecutorPushed( address(0), _executor );
+    }
+
+    function executor() public view override returns (address) {
+        return _executor;
+    }
+
+    modifier onlyExecutor() {
+        require( _executor == msg.sender, "Ownable: caller is not the owner" );
+        _;
+    }
+
+    function pushExecutor( address newExecutor_ ) public virtual override onlyExecutor() {
+        require( newExecutor_ != address(0), "Ownable: new owner is the zero address");
+        emit ExecutorPushed( _executor, newExecutor_ );
+        _newExecutor = newExecutor_;
+    }
+    
+    function pullExecutor() public virtual override {
+        require( msg.sender == _newExecutor, "Ownable: must be new owner to pull");
+        emit ExecutorPulled( _executor, _newExecutor );
+        _executor = _newExecutor;
+    }
+}
+
+interface IERC20 {
+    function decimals() external view returns (uint8);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function totalSupply() external view returns (uint256);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+library SafeERC20 {
+    using SafeMath for uint256;
+    using Address for address;
+
+    function safeTransfer(IERC20 token, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    }
+
+    function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+
+    function _callOptionalReturn(IERC20 token, bytes memory data) private {
+        bytes memory returndata = address(token).functionCall(data, "SafeERC20: low-level call failed");
+        if (returndata.length > 0) { // Return data is optional
+            // solhint-disable-next-line max-line-length
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
+    }
+}
+
+interface IBondCalculator {
+  function valuation( address pair_, uint amount_ ) external view returns ( uint _value );
+}
+
+contract OlympusTreasury is Queueable, Executable {
+
+    /* ========== DEPENDENCIES ========== */
+
+    using SafeMath for uint;
+    using SafeERC20 for IERC20;
+
+    /* ========== EVENTS ========== */
+
+    event Managed( address indexed token, uint amount );
+    event Dispensed( address indexed caller, address indexed recipient, uint amount );
+    event ChangeQueued( MANAGING indexed managing, address queued );
+    event ChangeActivated( MANAGING indexed managing, address activated, bool result );
+
+    /* ========== STATE VARIABLES ========== */
+
+    IERC20 immutable OHM;
+    uint public immutable blocksNeededForQueue;
+
+    address[] public reserveTokens; // Push only, beware false-positives.
+    mapping( address => bool ) public isReserveToken;
+    mapping( address => uint ) public reserveTokenQueue; // Delays changes to mapping.
+
+    address[] public liquidityTokens; // Push only, beware false-positives.
+    mapping( address => bool ) public isLiquidityToken;
+    mapping( address => uint ) public LiquidityTokenQueue; // Delays changes to mapping.
+
+    mapping( address => address ) public bondCalculator; // bond calculator for liquidity token
+
+    address[] public managers; // Push only, beware false-positives. Only for viewing.
+    mapping( address => bool ) public isManager;
+    mapping( address => uint ) public ManagerQueue; // Delays changes to mapping.
+
+    address[] public dispensees; // Push only, beware false-positives. Only for viewing.
+    mapping( address => bool ) public isDispensee;
+    mapping( address => uint ) public dispenseeQueue; // Delays changes to mapping.
+
+    /* ========== CONSTRUCTOR ========== */
+    
+    constructor (
+        address _OHM,
+        uint _blocksNeededForQueue
+    ) {
+        require( _OHM != address(0) );
+        OHM = IERC20( _OHM );
+
+        blocksNeededForQueue = _blocksNeededForQueue;
+    }
+
+    /* ========== INTERACTION FUNCTIONS ========== */
+
+    /**
+        @notice allow approved address to withdraw assets
+        @param _token address
+        @param _amount uint
+     */
+    function manage( address _token, uint _amount ) external {
+        require( isManager[ msg.sender ], "Not approved" );
+
+        IERC20( _token ).safeTransfer( msg.sender, _amount );
+
+        emit Managed( _token, _amount );
+    }
+
+    /**
+        @notice send epoch reward to staking contract
+     */
+    function dispense( address _recipient, uint _amount ) external {
+        require( isDispensee[ msg.sender ], "Not approved" );
+
+        OHM.transfer( _recipient, _amount );
+
+        emit Dispensed( msg.sender, _recipient, _amount );
+    } 
+
+    /* ========== MUTABLE FUNCTIONS ========== */
+
+    enum MANAGING { RESERVETOKEN, LIQUIDITYTOKEN, MANAGER, DISPENSEE }
+
+    /**
+        @notice queue address to change boolean in mapping
+        @param _managing MANAGING
+        @param _address address
+        @return bool
+     */
+    function queue( MANAGING _managing, address _address ) external onlyQueuer() returns ( bool ) {
+        require( _address != address(0) );
+        if ( _managing == MANAGING.RESERVETOKEN ) { // 0
+            reserveTokenQueue[ _address ] = block.number.add( blocksNeededForQueue );
+        } else if ( _managing == MANAGING.LIQUIDITYTOKEN ) { // 1
+            LiquidityTokenQueue[ _address ] = block.number.add( blocksNeededForQueue );
+        } else if ( _managing == MANAGING.MANAGER ) { // 2
+            ManagerQueue[ _address ] = block.number.add( blocksNeededForQueue );
+        } else if ( _managing == MANAGING.DISPENSEE ) { // 3
+            dispenseeQueue[ _address ] = block.number.add( blocksNeededForQueue );
+        } else return false;
+
+        emit ChangeQueued( _managing, _address );
+        return true;
+    }
+
+    /**
+        @notice verify queue then set boolean in mapping
+        @param _managing MANAGING
+        @param _address address
+        @param _calculator address
+        @return bool
+     */
+    function toggle( MANAGING _managing, address _address, address _calculator ) external onlyExecutor() returns ( bool ) {
+        require( _address != address(0) );
+        bool result;
+        if ( _managing == MANAGING.RESERVETOKEN ) { // 0
+            if ( requirements( reserveTokenQueue, isReserveToken, _address ) ) {
+                reserveTokenQueue[ _address ] = 0;
+                if( !listContains( reserveTokens, _address ) ) {
+                    reserveTokens.push( _address );
+                }
+            }
+            result = !isReserveToken[ _address ];
+            isReserveToken[ _address ] = result;
+
+        } else if ( _managing == MANAGING.LIQUIDITYTOKEN ) { // 1
+            if ( requirements( LiquidityTokenQueue, isLiquidityToken, _address ) ) {
+                LiquidityTokenQueue[ _address ] = 0;
+                if( !listContains( liquidityTokens, _address ) ) {
+                    liquidityTokens.push( _address );
+                }
+            }
+            result = !isLiquidityToken[ _address ];
+            isLiquidityToken[ _address ] = result;
+            bondCalculator[ _address ] = _calculator;
+
+        } else if ( _managing == MANAGING.MANAGER ) { // 2
+            if ( requirements( ManagerQueue, isManager, _address ) ) {
+                ManagerQueue[ _address ] = 0;
+                if( !listContains( managers, _address ) ) {
+                    managers.push( _address );
+                }
+            }
+            result = !isManager[ _address ];
+            isManager[ _address ] = result;
+
+        } else if ( _managing == MANAGING.DISPENSEE ) { // 3
+            if ( requirements( dispenseeQueue, isDispensee, _address ) ) {
+                dispenseeQueue[ _address ] = 0;
+                if( !listContains( dispensees, _address ) ) {
+                    dispensees.push( _address );
+                }
+            }
+            result = !isDispensee[ _address ];
+            isDispensee[ _address ] = result;
+
+        } else return false;
+
+        emit ChangeActivated( _managing, _address, result );
+        return true;
+    }
+
+    /* ========== VIEW FUNCTIONS ========== */
+
+    /**
+        @notice returns OHM valuation of asset
+        @param _token address
+        @param _amount uint
+        @return value_ uint
+     */
+    function valueOf( address _token, uint _amount ) public view returns ( uint value_ ) {
+        if ( isReserveToken[ _token ] ) {
+            // convert amount to match OHM decimals
+            value_ = _amount.mul( 10 ** OHM.decimals() ).div( 10 ** IERC20( _token ).decimals() );
+        } else if ( isLiquidityToken[ _token ] ) {
+            value_ = IBondCalculator( bondCalculator[ _token ] ).valuation( _token, _amount );
+        }
+    }
+
+    /**
+        @notice checks requirements and returns altered structs
+        @param queue_ mapping( address => uint )
+        @param status_ mapping( address => bool )
+        @param _address address
+        @return bool 
+     */
+    function requirements( 
+        mapping( address => uint ) storage queue_, 
+        mapping( address => bool ) storage status_, 
+        address _address 
+    ) internal view returns ( bool ) {
+        if ( !status_[ _address ] ) {
+            require( queue_[ _address ] != 0, "Must queue" );
+            require( queue_[ _address ] <= block.number, "Queue not expired" );
+            return true;
+        } return false;
+    }
+
+    /**
+        @notice checks array to ensure against duplicate
+        @param _list address[]
+        @param _token address
+        @return bool
+     */
+    function listContains( address[] storage _list, address _token ) internal view returns ( bool ) {
+        for( uint i = 0; i < _list.length; i++ ) {
+            if( _list[ i ] == _token ) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
