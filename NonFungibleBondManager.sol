6f09bfd1b175c4bed5be4247b3936dd82e88005c
FinagleLord <72840971+FinagleLord@users.noreply.github.com>
2021-09-28 19:45:47 -0500

    Update NonFungibleBondManager.sol

--- NonFungibleBondManager.sol
+++ NonFungibleBondManager.sol
@@ -393,143 +393,136 @@ contract NonFungibleToken is ERC165, IERC721, IERC721Metadata {
     ) internal virtual {}
 }
 
-interface IwsOHM {
-    function unwrap( uint _amount ) external returns ( uint );
-    function wrap( uint _amount ) external returns ( uint );
-    function approve(address spender, uint256 amount) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
-    function sOHMValue( uint _amount ) external view returns ( uint );
-    function wOHMValue( uint _amount ) external view returns ( uint );
-}
 
-interface IsOHM {
-    function approve(address spender, uint256 amount) external returns (bool);
-}
+// @author Dionysus
+contract NonFungibleBondManager is NonFungibleToken("Olympus Bond", "BOND"), Ownable {
 
-interface IStaking {
-    function stake( uint _amount, address _recipient ) external returns ( bool );
-    function unstake( uint _amount, bool _trigger ) external;
-    function claim( address _recipient ) external;
-}
+    /////////////// imports  ///////////////
 
-
-
-contract NonFungibleYeildManager is Ownable, NonFungibleToken("Olypus Yield Token", "YI3LD") {
-
-    /////////// imports ///////////
 
     using SafeERC20 for IERC20;
 
 
 
-    /////////// structs ///////////
 
-    struct Receipt {
-        address depositor;
-        uint amount;
-        uint vestingTerm;
-        uint inititialStakedOhmValue;
-    }
+    /////////////// storage  ///////////////
+
+
+    // Mapping from token id to the bond id it represents
+    mapping ( uint => uint ) public tokenToBond;
+
+    // Mapping from token id to underlying bond depository
+    mapping ( uint => address) public tokenToBondDepo;
+
+    // Mapping that returns if an address is a valid bond depo
+    mapping ( address => bool ) public whitelistedDepos;
+
+    // Mapping containig an array of bids for each bond
+    mapping ( uint => Bid[] ) public tokenBids;
+
+    // Mappping containing the last redemption timestamp for each bond
+    mapping ( uint => uint ) public lastRedeem;
+
+    // count of NonFungible bonds that have been minted, used to get the next bond index within tokenToBond mapping
+    uint public bondCount;
+
+
+
+
+    /////////////// structs ///////////////
+
 
     struct Bid {
         address bidder;     // owner of the bid
         address principal;  // principal token used as payment
         uint amount;        // amount of principal being paid
+        uint lastRedeem;    // used to determine if a bid is still valid or not
         bool refunded;      // has bidder refunded their bid
         uint deadline;      // time when bid is no longer acceptable
     }
 
 
 
-    /////////// storage ///////////
 
-    IwsOHM public wsOHM;
+    /////////////// events ///////////////
 
-    IStaking public staking;
 
-    uint public tokenCount;
+    event BondMinted ( 
+        address bondDepo, 
+        uint amount, 
+        uint maxPrice, 
+        address depositor, 
+        uint tokenId, 
+        uint bondId 
+    );
+    
+    
+    
 
-    mapping( uint => Receipt ) tokenToReceipt;
+    /////////////// policy  ///////////////
 
-    // Mapping containig an array of bids for each token
-    mapping ( uint => Bid[] ) public tokenBids;
 
-    ///////////  user  ///////////
+    function setValidDepo(address depo, bool isValid) external onlyPolicy() {
+        whitelistedDepos[ depo ] = isValid;
+    }
+
+
+    /////////////// bond logic  ///////////////
+
 
     function deposit(
-        address depositor,
-        uint amount,
-        uint vestingTerm
-    ) external returns ( uint tokenId, uint sOHMValue ) {
-        // interface the new available token index
-        Receipt storage receipt = tokenToReceipt [ tokenCount ];
-        // transfer users OHM to contract
-        IERC20( address( wsOHM ) ).safeTransferFrom( msg.sender, address(this), amount );
-        // set token id for return before its updated
-        tokenId = tokenCount;
-        // log receipt info
-        receipt.depositor = depositor;
-        receipt.amount = amount;
-        receipt.vestingTerm = vestingTerm;
-        // get sOHM value of depositors wsOHM for return
-        sOHMValue = wsOHM.sOHMValue( amount );
-        // store amount to be paid back to depositor after vestingTerm
-        receipt.inititialStakedOhmValue = sOHMValue;        
+        address _bondDepo,
+        uint _amount,
+        uint _maxPrice,
+        address _depositor
+    ) external returns ( uint payout, uint tokenId, uint bondId ) {
+        // make sure bond depo is valid
+        require( whitelistedDepos[ _bondDepo ] == true, "invalid depo");
+        // interface and deposit to bond depo
+        (payout, bondId) = IUniqueBondDepository( _bondDepo ).deposit( _amount, _maxPrice, address( this ) );
+        // mint user a NFT that represents their ownership of a unique bond
+        _safeMint(_depositor, bondCount);
+        // map the nft to the newly created bonds id 
+        tokenToBond[bondCount] = bondId;
+        // map the nft to its relevant depo for redeeming/tranfering ownership
+        tokenToBondDepo[bondCount] = _bondDepo;
+        // set tokenId and increment bondCount by one
+        tokenId = bondCount;
+        bondCount += 1;
+        // emit event with relevant details
+        emit BondMinted ( _bondDepo, _amount, _maxPrice, _depositor , tokenId, bondId);
     }
 
     function redeem( 
         uint _tokenId
     ) external returns ( uint payout, bool fullyVested ) {
-        // make sure token exists
         require( _exists( _tokenId ) );
-        // interface the tokens Receipt
-        Receipt storage receipt = tokenToReceipt [ _tokenId ];
-        // make sure caller owns the token or is the orginal depositor
-        require( ownerOf[ _tokenId ] == msg.sender || msg.sender == receipt.depositor, "You're not the owner");
-        // get the current value of the minters deposited wsOHM denominated in sOHM
-        uint currentValueInSOHM = wsOHM.sOHMValue( receipt.amount );
-        // make sure there's sOHM to be claimed
-        require( receipt.inititialStakedOhmValue < currentValueInSOHM,  "nothing to claim");
-        // calculate payout in sOHM
-        uint payoutInSOHM = currentValueInSOHM - receipt.inititialStakedOhmValue;
-        // calculate payout in wsOHM
-        payout = wsOHM.wOHMValue( payoutInSOHM );
-        // subtract the wsOHM that was paid out from the minters deposit amount 
-        receipt.amount -= payout;
-        // make sure depositor still has at least their original deposit denominated in staked ohm
-        require( receipt.amount >= receipt.inititialStakedOhmValue, "math error" );
-        // transfer token holder their payout
-        IERC20( address( wsOHM ) ).safeTransfer( ownerOf[ _tokenId ], payout);
-        // if vesting is compete 
-        if ( receipt.vestingTerm >= block.timestamp ) {
-            // burn the token
-            _burn( _tokenId );
-            // return fully vested
-            fullyVested = true;
-            // return a payout of 0 since vesting is over
-            payout = 0;
-            // transfer depositor their wsOHM back
-            IERC20( address( wsOHM ) ).safeTransfer( receipt.depositor, receipt.amount);
-        } 
+        require( msg.sender == ownerOf[ _tokenId ], "You're not the owner");
+        // redeem bond payout from relevent depository with payout sent to its owner
+        ( payout, fullyVested ) = IUniqueBondDepository(tokenToBondDepo[ _tokenId ] ).redeem( tokenToBond[ _tokenId ], ownerOf[ _tokenId ] );
+        // if fullyVested burn the bonds NFT
+        if ( fullyVested ) _burn( _tokenId );
+        // log the redemption time
+        lastRedeem[ _tokenId ] = block.timestamp;
     }
-
 
     function bid(
         uint _tokenId,
         address _principal,
-        uint _amount,
+        uint _amount, 
         uint _deadline
     ) external {
-        // make sure token exists
+        // make sure bond exists
         require( _exists( _tokenId ), "Bond doesn't exist" );
-        // interface tokens bids array
+        // interface bonds bids array
         Bid[] storage bids = tokenBids[ _tokenId ];
         // push bid to storage
         bids.push(
             Bid({
                 bidder: msg.sender,
                 principal: _principal,
-                amount: _amount,
+                amount: _amount, 
+                lastRedeem: lastRedeem[ _tokenId ], 
                 refunded: false,
                 deadline: _deadline
             })
@@ -542,19 +535,19 @@ contract NonFungibleYeildManager is Ownable, NonFungibleToken("Olypus Yield Toke
         uint _tokenId,
         uint _bidId
     ) external {
-        // make sure token exists
-        require( _exists( _tokenId ), "Token doesn't exist" );
-        // make sure caller is owner of the token
+        // make sure bond exists
+        require( _exists( _tokenId ), "Bond doesn't exist" );
+        // make sure caller is owner of the bond
         require( msg.sender == ownerOf[ _tokenId ], "You're not the owner");
-        // interface tokens array of bids
+        // interface bonds array of bids
         Bid[] storage bids = tokenBids[ _tokenId ];
         // interface bid thats being accepted
         Bid storage acceptedBid = bids[ _bidId ];
-        // ensure the deadline isn't over
-        require( acceptedBid.deadline >= block.timestamp, "Bid's invalid");
-        // transfer bid payment to tokens owner
+        // ensure the bond hasn't been redeemed since the offer was created
+        require( acceptedBid.lastRedeem == lastRedeem[ _tokenId ], "Bid's invalid");
+        // transfer bid payment to bonds owner
         IERC20( acceptedBid.principal ).safeTransfer( ownerOf[ _tokenId ], acceptedBid.amount );
-        // transfer token to its new owner
+        // transfer bond to its new owner
         safeTransferFrom(ownerOf[ _tokenId ], acceptedBid.bidder, _tokenId);
     }
 
@@ -562,11 +555,11 @@ contract NonFungibleYeildManager is Ownable, NonFungibleToken("Olypus Yield Toke
         uint _tokenId,
         uint _bidId
     ) external {
-        // make sure token exists
-        require( _exists( _tokenId ), "Token doesn't exist" );
-        // make sure caller is owner of the token
+        // make sure bond exists
+        require( _exists( _tokenId ), "Bond doesn't exist" );
+        // make sure caller is owner of the bond
         require( msg.sender == ownerOf[ _tokenId ], "You're not the owner");
-        // interface tokens array of bids
+        // interface bonds array of bids
         Bid[] storage bids = tokenBids[ _tokenId ];
         // interface bid thats being cancelled
         Bid storage _bid = bids[ _bidId ];
